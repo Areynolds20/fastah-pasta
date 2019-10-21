e4e64cdb02727a29631ba9d7467083a666a90548
Areynolds20 <54539241+Areynolds20@users.noreply.github.com>
2019-10-21 08:19:55 -0500

    adfasdf

---
+++ toggle-menu.js
@@ -0,0 +1,7 @@
+function toggleMenu() {
+    if (document.getElementById("main-nav").style.display == "none") {
+        document.getElementById("main-nav").style.display = "block";
+    } else {
+        document.getElementById("main-nav").style.display = "none";
+    }
+}
